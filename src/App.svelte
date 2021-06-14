fc2ce7b2f95ac03095a1ab686210e0883c143f15
ishank.web.dev@gmail.com <ishank>
2021-06-14 13:55:24 +0530

    text area

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
-<script>
+<script lang="typescript">
   import Terminal from "./components/Terminal.svelte";
-</script>
-
-<div class="bg-white dark:bg-denim-900 w-screen h-screen text-denim-100 pt-8">
-  <Terminal />
-</div>
-
-<style lang="postcss">
+  
+</script> 
+  <div class="bg-white dark:bg-denim-900 w-screen h-screen text-denim-100 pt-8">
+    <Terminal username="ishank@123123"/>
+  </div>
+  
+  <style lang="postcss">
 </style>
--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -1,15 +1,25 @@
-<script lang="ts">
+<script lang="typescript">
   let cmdLets = [{}];
-  export let username :string;
-  let prompt = `${username}-:`;
+  export let username: string;
+  let prompt = `${username} :`;
+  let terminalRef:HTMLTextAreaElement ;
+
+  const handlePageClick = ()=>{
+    terminalRef.focus();
+  }
 </script>
+<svelte:window on:click={handlePageClick}/>
 
 <div
   class="md:container container sm:mx-auto px-4 py-6 bg-denim-500 dark:bg-denim-800 rounded"
 >
   {#each cmdLets as cmd}
-    <div>
-      {prompt}
+    <div class="flex flex-row gap-4">
+      <span class="flex-1 flex-initial">
+        {prompt}
+      </span>
+      <!-- svelte-ignore a11y-autofocus -->
+      <textarea bind:this={terminalRef} autofocus class="flex-1 w-full bg-denim-500 dark:bg-denim-800 border-none focus:outline-none"/>
     </div>
   {/each}
 </div>
