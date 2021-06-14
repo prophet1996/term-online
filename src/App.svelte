01ef1d3f656757e6fccc43001c5d52463b71d513
ishank.web.dev@gmail.com <ishank>
2021-06-14 18:42:50 +0530

    input resize like a boss

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Terminal from "./components/Terminal.svelte";
   
 </script> 
-  <div class="bg-white dark:bg-denim-900 w-screen h-screen text-denim-100 pt-8">
+  <div class="bg-white dark:bg-denim-900 w-screen h-screen text-denim-100 pt-8 cursor-text">
     <Terminal username="ishank@123123"/>
   </div>
   
--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -1,25 +1,66 @@
-<script lang="typescript">
-  let cmdLets = [{}];
+<script lang="ts">
+  import { onMount, tick } from "svelte";
+
   export let username: string;
   let prompt = `${username} :`;
-  let terminalRef:HTMLTextAreaElement ;
+  let terminalRef: HTMLTextAreaElement;
+  let cmdLets = [{ value: "" }];
 
-  const handlePageClick = ()=>{
+  onMount(() => {
+    terminalRef.style.height = "24px";
     terminalRef.focus();
-  }
-</script>
-<svelte:window on:click={handlePageClick}/>
+  });
+  const handlePageClick = () => {
+    terminalRef.focus();
+  };
 
-<div
-  class="md:container container sm:mx-auto px-4 py-6 bg-denim-500 dark:bg-denim-800 rounded"
->
-  {#each cmdLets as cmd}
-    <div class="flex flex-row gap-4">
+  const autoTerminalInputResize = () => {
+    const row = Math.min(
+      Math.ceil(
+        (terminalRef.value.length * 0.6 * 16) / terminalRef.offsetWidth
+      ),
+      20
+    );
+    console.log(row);
+    terminalRef.style.height = `${row * 24}px`;
+  };
+
+  const handleCmdKeyDown = async (event) => {
+    if (event.code === "Enter") {
+      event.preventDefault();
+      cmdLets = [...cmdLets, { value: "" }];
+      await tick();
+      terminalRef.focus();
+    }
+  };
+</script>
+
+<svelte:window on:click={handlePageClick} on:resize={autoTerminalInputResize} />
+
+{#each cmdLets as cmd, idx}
+  <div
+    class="md:container container sm:mx-auto px-4 py-6 bg-denim-500 dark:bg-denim-800 rounded"
+  >
+    <div class="flex flex-row gap-4 items-start">
       <span class="flex-1 flex-initial">
         {prompt}
       </span>
       <!-- svelte-ignore a11y-autofocus -->
-      <textarea bind:this={terminalRef} autofocus class="flex-1 w-full bg-denim-500 dark:bg-denim-800 border-none focus:outline-none"/>
+      {#if idx === cmdLets.length - 1}
+        <textarea
+          bind:this={terminalRef}
+          on:input={autoTerminalInputResize}
+          bind:value={cmd.value}
+          on:keydown={handleCmdKeyDown}
+          class="flex-1 w-full bg-denim-500 dark:bg-denim-800 border-none focus:outline-none resize-none"
+        />
+      {:else}
+        <textarea
+          disabled={true}
+          bind:value={cmd.value}
+          class="flex-1 w-full bg-denim-500 dark:bg-denim-800 border-none focus:outline-none resize-none"
+        />
+      {/if}
     </div>
-  {/each}
-</div>
+  </div>
+{/each}
